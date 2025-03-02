4c78f976461b86a2a7ac9abad163bec1b6952663
skar777123 <sanskargaikwad206@gmail.com>
2025-03-02 23:09:19 +0530

    changes 4

--- main.py
+++ main.py
@@ -5,8 +5,22 @@ from annoy import AnnoyIndex
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 import os
 warnings.filterwarnings('ignore')
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app = FastAPI()
 load_dotenv()
